105025450b2b5ce37d4ef844ff0dc3d4295ae4a7
h17lima <han.lima.19@gmail.com>
2025-09-08 01:54:03 -0400

    modelo User com Devise e modelo Note criados

---
+++ db/migrate/20250908054657_create_notes.rb
@@ -0,0 +1,12 @@
+class CreateNotes < ActiveRecord::Migration[8.0]
+  def change
+    create_table :notes do |t|
+      t.string :title
+      t.text :content
+      t.references :user, null: false, foreign_key: true
+      t.string :tags
+
+      t.timestamps
+    end
+  end
+end
